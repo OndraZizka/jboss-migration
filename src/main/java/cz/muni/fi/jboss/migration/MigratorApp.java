da1346c88fc9fc1a3b2bde485c4cfcf8899c9684
romanjakubco <roman.jakubco@gmail.com>
2013-01-29 23:46:00 +0100

    Basic implementation of Main for App.

--- src/main/java/cz/muni/fi/jboss/migration/MigratorApp.java
+++ src/main/java/cz/muni/fi/jboss/migration/MigratorApp.java
@@ -1,9 +1,6 @@
 package cz.muni.fi.jboss.migration;
 
-import cz.muni.fi.jboss.migration.ex.ApplyMigrationException;
-import cz.muni.fi.jboss.migration.ex.CliScriptException;
-import cz.muni.fi.jboss.migration.ex.CopyException;
-import cz.muni.fi.jboss.migration.ex.LoadMigrationException;
+import cz.muni.fi.jboss.migration.ex.*;
 import cz.muni.fi.jboss.migration.migrators.connectionFactories.ResAdapterMigrator;
 import cz.muni.fi.jboss.migration.migrators.dataSources.DatasourceMigrator;
 import cz.muni.fi.jboss.migration.migrators.logging.LoggingMigrator;
@@ -11,42 +8,27 @@ import cz.muni.fi.jboss.migration.migrators.security.SecurityMigrator;
 import cz.muni.fi.jboss.migration.migrators.server.ServerMigrator;
 import cz.muni.fi.jboss.migration.spi.IMigrator;
 import javafx.util.Pair;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 /**
+ * Main class of the application
+ *
  * @author Roman Jakubco
- *         Date: 1/24/13
- *         Time: 10:25 AM
  */
-//
-//Concole UI
-//        ==========
-//
-//        Usage:
-//
-//        java -jar AsMigrator.jar [<option>, ...] [as5.dir=]<as5.dir> [as7.dir=]<as7.dir>
-//
-//        Options:
-//
-//        as5.profile=<name>
-//Path to AS 5 profile.
-//        Default: "default"
-//
-//        as7.confPath=<path>
-//Path to AS 7 config file.
-//        Default: "standalone/conf/standalone.xml"
-//
-//        conf.<module>.<property>=<value> := Module-specific options.
-//
-//<module> := Name of one of modules. E.g. datasource, jaas, security, ...
-//<property> := Name of the property to set. Specific per module. May occur multiple times.
 
 public class MigratorApp {
     public static void main(String[] args) {
@@ -131,31 +113,106 @@ public class MigratorApp {
         global.setStandalonePath();
         configuration.setModuleOtions(moduleOptions);
         configuration.setOptions(global);
+        Migrator migrator;
+        MigrationContext ctx;
+        Document nonAlteredStandalone;
+
 
         try {
-            MigrationContext ctx = new MigrationContext();
+            ctx = new MigrationContext();
             ctx.createBuilder();
-            Migrator migrator = new Migrator(configuration, ctx);
+            File standalone = new File(configuration.getGlobal().getStandaloneFilePath());
+
+            Document doc = ctx.getDocBuilder().parse(standalone);
+            nonAlteredStandalone = ctx.getDocBuilder().parse(standalone);
+            ctx.setStandaloneDoc(doc);
+
+            migrator = new Migrator(configuration, ctx);
 
             migrator.loadAS5Data();
-            migrator.apply();
+        } catch (ParserConfigurationException | LoadMigrationException | SAXException | IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            migrator.getDOMElements();
+        } catch (MigrationException e) {
+            System.err.println(e.toString());
+            e.printStackTrace();
+            return;
+        }
+
+        // TODO: Where write scripts?
+        try {
+           for(String script : migrator.getCLIScripts()){
+               System.out.println(script);
+           }
+        } catch (CliScriptException e) {
+            System.err.println(e.toString());
+            e.printStackTrace();
+            return;
+        }
+
+        try {
             migrator.copyItems();
-            for(String s : migrator.getCLIScripts()){
-                System.out.println(s);
-            }
-
-
-        } catch (LoadMigrationException | CliScriptException | ApplyMigrationException | CopyException e) {
+        } catch (CopyException e) {
+            System.err.println(e.toString());
             e.printStackTrace();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } finally {
-            // Reverse copying
+            removeData(ctx.getCopyMemories());
+            FileUtils.deleteQuietly(new File(global.getDirAS7() + File.separator + "modules" + File.separator + "jdbc"));
+            return;
+        }
+
+        try {
+            migrator.apply();
+        } catch (ApplyMigrationException e) {
+            System.err.println(e.toString());
+            cleanStandalone(nonAlteredStandalone, configuration);
+            e.printStackTrace(); removeData(ctx.getCopyMemories());
+            FileUtils.deleteQuietly(new File(global.getDirAS7() + File.separator + "modules" + File.separator + "jdbc"));
+            return;
         }
     }
 
-    public static void writeHelp(){
+    /**
+     * Helping method for writing help.
+     */
+    private static void writeHelp(){
 
+    }
+
+    /**
+     * Method for removing copied required data for migration from AS5 server if the app fails.
+     *
+     * @param copyMemories files, which where copied to AS7 folder
+     */
+    private static void removeData(Collection<CopyMemory> copyMemories){
+        for(CopyMemory cp : copyMemories){
+            if(!(cp.getType().equals("driver"))){
+                FileUtils.deleteQuietly(new File(cp.getTargetPath() + File.separator + cp.getName()));
+            }
+        }
+    }
+
+    /**
+     * Method for returning standalone file to its original state before migration if the app fails.
+     *
+     * @param doc object of Document representing original standalone file
+     * @param config configuration of app
+     */
+    private static void cleanStandalone(Document doc, Configuration config){
+        try {
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            StreamResult result = new StreamResult(new File(config.getGlobal().getStandaloneFilePath()));
+
+            DOMSource source = new DOMSource(doc);
+            transformer.transform(source, result);
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
     }
 
     // TODO: Name of the oracle driver is ojdbc.jar. What is problem. Check all others drivers.
