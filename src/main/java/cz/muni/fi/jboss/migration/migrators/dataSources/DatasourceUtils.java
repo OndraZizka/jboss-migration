d1b4bf4429986f117909836969c4da7a0f6fd661
OndraZizka <zizka@seznam.cz>
2013-03-27 20:06:44 +0100

    Roman's comment on DatasourceUtils

--- src/main/java/cz/muni/fi/jboss/migration/migrators/dataSources/DatasourceUtils.java
+++ src/main/java/cz/muni/fi/jboss/migration/migrators/dataSources/DatasourceUtils.java
@@ -4,8 +4,21 @@ import cz.muni.fi.jboss.migration.RollbackData;
 import org.apache.commons.lang.StringUtils;
 
 /**
- *
- * @author Ondrej Zizka, ozizka at redhat.com
+ * Roman:
+ * Tieto dve metody podla mna su uplne zbytocne v buducnosti. 
+ * Toto fungovalo len na oko s tym, ze som povedal ze sa budu len tieto drivery migrovat. 
+ * Omnoho lepsie by bolo pouzit techniku z loggingu. 
+ * Teda pouzit metodu na prehladavanie JAR files a najdenie v nich classu, 
+ * ktora je uvedena v datasourcoch a podla toho setnut rollback data. A presunut ju do AS7 a vytvorit module.
+ * 
+ * Tym padom by odpadlo aj setovanie alternatvneho driveru lebo aplikacia by snad nasla jar toho alternativneho driveru
+ * (snad nebudu dve rozlisne jar files s rovnakou classou...). 
+ * Nepremenil som to zatial lebo som si nebol isty o spravnosti a tvojom nazore na prehladavanie 
+ * skoro vsetkych JAR files v AS5 a hladanie v nich odpovedajucu classu uz v loggingu a nie to este tu.
+ * 
+ * Tym padom by metoda deriveAndSetDriverName stratila zmysel lebo by sa rovno setlo meno jarka v rollbackData. No a na vymyslanie nazvu modulu by sa dalo vymysliet nieco praktickejsie, co by sa odvodzovalo od mena samotneho JAR filu alebo umely nazov. 
+ * 
+ * @author Roman Jakubco
  */
 public class DatasourceUtils {
     
