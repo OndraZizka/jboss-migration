6fb6df6a3ca7bbdaa7387e65eb1ba0ce8ebceeef
OndraZizka <zizka@seznam.cz>
2013-04-04 19:48:00 +0200

    Javadoc for IMigrationAction.

--- src/main/java/cz/muni/fi/jboss/migration/actions/IMigrationAction.java
+++ src/main/java/cz/muni/fi/jboss/migration/actions/IMigrationAction.java
@@ -11,6 +11,11 @@ import java.util.List;
  */
 public interface IMigrationAction {
     
+    /**
+     *  State management - basically, a helper state to check in case 
+     *   the lifecycle methods were called in wrong order.
+     *  Should be only used by actions themselves.
+     */
     enum State { INITIAL, BACKED_UP, DONE, FINISHED, ROLLED_BACK };
 
     State getState();
@@ -28,16 +33,41 @@ public interface IMigrationAction {
     
     // "Lifecycle"
     
+    /**
+     *  Checks whether this action can be performed under current conditions.
+     *  E.g. action for file copying would check if the file exists and is readable,
+     *  and if the destination file does not exists or is allowed to be overwritten and is writable.
+     */
     void preValidate() throws MigrationException;
     
+    /**
+     *  Performs whatever is necessary to roll back the action after it was performed.
+     *  Data are stored in this action object.
+     *  Data stored by this method will be used in rollback().
+     */
     void backup() throws MigrationException;
     
+    /**
+     *  Actually performs the action - does the real change: 
+     *  Copies the file, sends a CLI command.
+     */
     void perform() throws MigrationException;
     
+    /**
+     *  Checks if the actions went well, e.g check if the file was copied etc.
+     *  May be no-op for certain operations which went well if perform() didn't throw.
+     */
     void postValidate() throws MigrationException;
     
+    /**
+     *  Cleans anything created by the backup() method, e.g a backup file.
+     */
     void cleanBackup();
     
+    /**
+     *  Undoes whatever was done by perform(). Uses data stored in this action by backup().
+     * @throws MigrationException 
+     */
     void rollback() throws MigrationException;
     
-}
+}// class
