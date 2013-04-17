189bef69f0634bd09aa89a4b2eb076dd0280d96e
OndraZizka <zizka@seznam.cz>
2013-04-17 02:26:52 +0200

    Add MergePropertiesFilesAction, create common super class with CopyAction

--- src/main/java/cz/muni/fi/jboss/migration/actions/CopyAction.java
+++ src/main/java/cz/muni/fi/jboss/migration/actions/CopyAction.java
@@ -6,52 +6,43 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Objects;
 
 /**
  * @author Ondrej Zizka, ozizka at redhat.com
  */
-public class CopyAction extends AbstractStatefulAction {
+public class CopyAction extends FileAbstractAction {
 
-    private File src;
-    private File dest;
-    private File temp;
     private boolean overwrite;
-    private boolean failIfNotExist = true;
 
 
     public CopyAction(File src, File dest, boolean overwrite) {
-        this.src = src;
-        this.dest = dest;
+        super(src, dest);
         this.overwrite = overwrite;
     }
 
 
     public CopyAction( File src, File dest, boolean overwrite, boolean failIfNotExist) {
-        this.src = src;
-        this.dest = dest;
+        super( src, dest, failIfNotExist );
         this.overwrite = overwrite;
-        this.failIfNotExist = failIfNotExist;
     }
 
     
-    
-    @Override
-    public String toDescription() {
-        return "Copy file, may" + (this.overwrite ? "" : " not") + " overwrite, "
-                + (this.failIfNotExist ? "" : "don't ") + "fail if exists,"
-                + "\n  from " + this.src.getPath()
-                + "\n    to " + this.dest.getPath();
+    @Override protected String verb() {
+        return "Merge";
     }
     
+    @Override
+    public String addToDescription() {
+        return "may" + (this.overwrite ? "" : " not") + " overwrite, ";
+    }
     
     
 
     @Override
     public void preValidate() throws MigrationException {
-        if ( ! src.exists() && failIfNotExist )
+        if( ! src.exists() && failIfNotExist )
             throw new ActionException(this, "File to copy doesn't exist: " + src.getPath());
-        if (dest.exists() && !overwrite)
+        if( src.exists() && !overwrite)
             throw new ActionException(this, "Copy destination exists, overwrite not allowed: " + dest.getAbsolutePath());
     }
 
@@ -59,93 +50,11 @@ public class CopyAction extends AbstractStatefulAction {
     @Override
     public void perform() throws MigrationException {
         try {
-            FileUtils.copyFile(src, dest);
+            FileUtils.copyFile( src, dest );
             setState(State.DONE);
         } catch (IOException ex) {
             throw new ActionException(this, "Copying failed: " + ex.getMessage(), ex);
         }
     }
-
-
-    @Override
-    public void rollback() throws MigrationException {
-        if (this.isAfterPerform()) {
-            if (this.overwrite && this.temp != null) {
-                try {
-                    FileUtils.copyFile(this.temp, this.dest);
-                } catch (IOException e) {
-                    throw new ActionException(this, "Restoring the previous file failed: " + e.getMessage(), e);
-                }
-            } else {
-                FileUtils.deleteQuietly(this.dest);
-            }
-        }
-        // Replace copied file with backup.
-
-        setState(State.ROLLED_BACK);
-    }
-
-
-    @Override
-    public void postValidate() throws MigrationException {
-        // Empty - JRE would give IOEx if something.
-    }
-
-
-    @Override
-    public void backup() throws MigrationException {
-        // Copy dest, if exists and overwrite allowed, to a temp file.
-        if (this.dest.exists() && this.overwrite) {
-            try {
-                this.temp = File.createTempFile(this.dest.getName(), null);
-                FileUtils.copyFile(this.dest, this.temp);
-            } catch (IOException ex) {
-                throw new ActionException(this, "Creating a backup file failed: " + ex.getMessage(), ex);
-            }
-        }
-        setState(State.BACKED_UP);
-    }
-
-
-    @Override
-    public void cleanBackup() {
-        // Remove temp file.
-        if ((!this.isAfterBackup()) || (!this.overwrite))
-            return;
-        if (this.temp.exists()) {
-            FileUtils.deleteQuietly(this.temp);
-        }
-        setState(State.FINISHED);
-    }
-
-
-    //<editor-fold defaultstate="collapsed" desc="hash/eq - use src and dest.">
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 67 * hash + Objects.hashCode( this.src );
-        hash = 67 * hash + Objects.hashCode( this.dest );
-        return hash;
-    }
-    
-    
-    @Override
-    public boolean equals( Object obj ) {
-        if( obj == null ) {
-            return false;
-        }
-        if( getClass() != obj.getClass() ) {
-            return false;
-        }
-        final CopyAction other = (CopyAction) obj;
-        if( !Objects.equals( this.src, other.src ) ) {
-            return false;
-        }
-        if( !Objects.equals( this.dest, other.dest ) ) {
-            return false;
-        }
-        return true;
-    }
-    //</editor-fold>
 
 }// class
