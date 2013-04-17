64d24a129b4b3ab4284ce23a0115e64110b56834
OndraZizka <zizka@seznam.cz>
2013-04-17 05:35:19 +0200

    Versions bump; Add Arquillian to the project

---
+++ src/test/java/cz/muni/fi/jboss/migration/ArqTest.java
@@ -0,0 +1,19 @@
+package cz.muni.fi.jboss.migration;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author Ondrej Zizka, ozizka at redhat.com
+ */
+@RunWith(Arquillian.class)
+public class ArqTest {
+    
+    @Test
+    public void testLiveMigration(){
+        
+    }
+    
+}// class
