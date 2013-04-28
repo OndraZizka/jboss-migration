b2ad3c5c73f50920ef3020c8ab5269f349a9cd75
OndraZizka <zizka@seznam.cz>
2013-04-29 00:24:46 +0200

    Add announceMigration()

--- src/test/java/org/jboss/loom/ArqTest.java
+++ src/test/java/org/jboss/loom/ArqTest.java
@@ -40,6 +40,7 @@ public class ArqTest {
                 
         return conf;
     }
+    
     private static Configuration createTestConfig_EAP_520_production() throws IOException {
         Configuration conf = new Configuration();
         
@@ -106,7 +107,7 @@ public class ArqTest {
     public void test_EAP_520_production( ) throws Exception {
         System.out.println( "doMigration" );
                 
-        Configuration conf = createTestConfig_AS_510_all();
+        Configuration conf = createTestConfig_EAP_520_production();
         AS7Config as7Config = conf.getGlobal().getAS7Config();
         
         ModelControllerClient as7client = ModelControllerClient.Factory.create(as7Config.getHost(), as7Config.getManagementPort());
@@ -125,14 +126,15 @@ public class ArqTest {
     }
     
     
-    private static String announceMigration( Configuration conf ){
-        return "\n\n"
+    private static void announceMigration( Configuration conf ){
+        String msg = "\n\n"
                 + "==========================================================="
                 + "  Migrating "
                 + "  " + conf.getGlobal().getAS5Config().getDir() + " | " + conf.getGlobal().getAS5Config().getProfileName()
                 + "  to "
                 + "   " + conf.getGlobal().getAS7Config().getDir() + " | " + conf.getGlobal().getAS7Config().getConfigPath()
                 + "===========================================================\n";
+        System.out.println( msg );
     }
     
 }// class
