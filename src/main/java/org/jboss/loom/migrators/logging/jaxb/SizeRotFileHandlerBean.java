49cc9da3b1e6328ea23eb24e91a58c56689737ef
OndraZizka <zizka@seznam.cz>
2013-05-25 03:38:19 +0200

    Introduce HandlerBeanBase to have getName in common.

--- src/main/java/org/jboss/loom/migrators/logging/jaxb/SizeRotFileHandlerBean.java
+++ src/main/java/org/jboss/loom/migrators/logging/jaxb/SizeRotFileHandlerBean.java
@@ -8,7 +8,6 @@
 package org.jboss.loom.migrators.logging.jaxb;
 
 import org.eclipse.persistence.oxm.annotations.XmlPath;
-
 import javax.xml.bind.annotation.*;
 
 /**
@@ -16,15 +15,10 @@ import javax.xml.bind.annotation.*;
  *
  * @author Roman Jakubco
  */
-
 @XmlRootElement(name = "size-rotating-handler")
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlType(name = "size-rotating-handler")
-
-public class SizeRotFileHandlerBean {
-
-    @XmlAttribute(name = "name")
-    private String name;
+public class SizeRotFileHandlerBean extends HandlerBeanBase {
 
     @XmlPath("encoding/@value")
     private String encoding;
@@ -56,91 +50,24 @@ public class SizeRotFileHandlerBean {
     @XmlPath("max-backup-index/@value")
     private String maxBackupIndex;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLevel() {
-        return level;
-    }
-
-    public void setLevel(String level) {
-        this.level = level;
-    }
-
-    public String getFilter() {
-        return filter;
-    }
-
-    public void setFilter(String filter) {
-        this.filter = filter;
-    }
-
-    public String getFormatter() {
-        return formatter;
-    }
-
-    public void setFormatter(String formatter) {
-        this.formatter = formatter;
-    }
-
-    public String getAutoflush() {
-        return autoflush;
-    }
-
-    public void setAutoflush(String autoflush) {
-        this.autoflush = autoflush;
-    }
-
-    public String getAppend() {
-        return append;
-    }
-
-    public void setAppend(String append) {
-        this.append = append;
-    }
-
-    public String getRelativeTo() {
-        return relativeTo;
-    }
-
-    public void setRelativeTo(String relativeTo) {
-        this.relativeTo = relativeTo;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public String getRotateSize() {
-        return rotateSize;
-    }
-
-    public void setRotateSize(String rotateSize) {
-        this.rotateSize = rotateSize;
-    }
-
-    public String getMaxBackupIndex() {
-        return maxBackupIndex;
-    }
-
-    public void setMaxBackupIndex(String maxBackupIndex) {
-        this.maxBackupIndex = maxBackupIndex;
-    }
-
-    public String getEncoding() {
-        return encoding;
-    }
-
-    public void setEncoding(String encoding) {
-        this.encoding = encoding;
-    }
+    public String getLevel() { return level; }
+    public void setLevel(String level) { this.level = level; }
+    public String getFilter() { return filter; }
+    public void setFilter(String filter) { this.filter = filter; }
+    public String getFormatter() { return formatter; }
+    public void setFormatter(String formatter) { this.formatter = formatter; }
+    public String getAutoflush() { return autoflush; }
+    public void setAutoflush(String autoflush) { this.autoflush = autoflush; }
+    public String getAppend() { return append; }
+    public void setAppend(String append) { this.append = append; }
+    public String getRelativeTo() { return relativeTo; }
+    public void setRelativeTo(String relativeTo) { this.relativeTo = relativeTo; }
+    public String getPath() { return path; }
+    public void setPath(String path) { this.path = path; }
+    public String getRotateSize() { return rotateSize; }
+    public void setRotateSize(String rotateSize) { this.rotateSize = rotateSize; }
+    public String getMaxBackupIndex() { return maxBackupIndex; }
+    public void setMaxBackupIndex(String maxBackupIndex) { this.maxBackupIndex = maxBackupIndex; }
+    public String getEncoding() { return encoding; }
+    public void setEncoding(String encoding) { this.encoding = encoding; }
 }
