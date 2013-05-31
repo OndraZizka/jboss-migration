8ac8f89655638e028bb60f7fca4a35ad75829dd7
OndraZizka <zizka@seznam.cz>
2013-05-31 08:00:13 +0200

    Add unmarshallBeans() to parse into a list.

--- src/main/java/org/jboss/loom/utils/XmlUtils.java
+++ src/main/java/org/jboss/loom/utils/XmlUtils.java
@@ -2,6 +2,8 @@ package org.jboss.loom.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -14,6 +16,7 @@ import org.jboss.loom.ex.MigrationException;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
@@ -24,22 +27,30 @@ public class XmlUtils {
     private static final org.slf4j.Logger log = LoggerFactory.getLogger( XmlUtils.class );
     
     public static <T> T unmarshallBean( File docFile, String xpath, Class<T> cls ) throws MigrationException{
+        return unmarshallBeans( docFile, xpath, cls ).get(0);
+    }
         
+    public static <T> List<T> unmarshallBeans( File docFile, String xpath, Class<T> cls ) throws MigrationException{
+        
+        List<T> beans = new LinkedList();
         DocumentBuilder docBuilder = Utils.createXmlDocumentBuilder();
         try {
             Document doc = docBuilder.parse(docFile);
 
             XPath xp = XPathFactory.newInstance().newXPath();
-            String exp = "/deployment/bean[@name='BootstrapProfileFactory']/property[@name='applicationURIs']//list[@elementClass='java.net.URI']";
-            Node node = (Node) xp.evaluate(exp, doc, XPathConstants.NODE);
-
+            NodeList nodes = (NodeList) xp.evaluate(xpath, doc, XPathConstants.NODESET);
+            
             Unmarshaller unmarshaller = JAXBContext.newInstance(cls).createUnmarshaller();
-            T bean = (T) unmarshaller.unmarshal(node);
-            return bean;
+            for( int i = 0; i < nodes.getLength(); i++ ) {
+                Node node = nodes.item( i );
+                T bean = (T) unmarshaller.unmarshal(node);
+                beans.add( bean );
+            }
         }
         catch( SAXException | IOException | XPathExpressionException | JAXBException ex ) {
             throw new MigrationException("Failed parsing bean from a XML file " + docFile.getPath() + ": " + ex.getMessage(), ex);
         }
+        return beans;
     }
     
 }// class
