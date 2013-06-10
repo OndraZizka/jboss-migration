81d707180c9e37d932de3490b3e928553789cd67
OndraZizka <zizka@seznam.cz>
2013-06-10 02:45:09 +0200

    MOXy - CCEx

--- src/main/java/org/jboss/loom/tools/report/beans/ConfigFragmentReportBean.java
+++ src/main/java/org/jboss/loom/tools/report/beans/ConfigFragmentReportBean.java
@@ -1,5 +1,8 @@
 package org.jboss.loom.tools.report.beans;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Map;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -8,11 +11,14 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import org.apache.commons.lang.StringUtils;
 import org.jboss.loom.migrators.HasProperties;
 import org.jboss.loom.migrators.Origin;
 import org.jboss.loom.spi.IConfigFragment;
 import org.jboss.loom.spi.ann.ConfigPartDescriptor;
 import org.jboss.loom.tools.report.adapters.MapPropertiesAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -21,6 +27,7 @@ import org.jboss.loom.tools.report.adapters.MapPropertiesAdapter;
 @XmlRootElement
 @XmlAccessorType( XmlAccessType.NONE )
 public class ConfigFragmentReportBean {
+    private static final Logger log = LoggerFactory.getLogger(ConfigFragmentReportBean.class);
     
     
     private ConfigPartDescriptor annotation;
@@ -52,8 +59,18 @@ public class ConfigFragmentReportBean {
             bean.origin = ((Origin.Wise)fragment).getOrigin();
         }
         
+        // Properties - own.
         if( fragment instanceof HasProperties ){
             bean.properties = ((HasProperties)fragment).getProperties();
+        }
+        // Properties - getters to a map.
+        else {
+            /*try {
+                bean.properties = BeanUtils.describe( fragment );
+            } catch(     IllegalAccessException | InvocationTargetException | NoSuchMethodException ex ) {
+                log.warn("Failed extracting properties from " + bean.getClass().getSimpleName() + ":\n    " + ex.getMessage(), ex );
+            }*/
+            bean.properties = describeBean( fragment );
         }
         
         return bean;
@@ -70,6 +87,29 @@ public class ConfigFragmentReportBean {
     
     
     private static String nullIfEmpty(String str){ return str == null ? null : (str.isEmpty() ? null : str); }
+    
+    
+    /**
+     *  Extracts all String getters properties to a map.
+     */
+    public static Map<String, String> describeBean(IConfigFragment bean){
+        
+        Map<String, String> ret = new HashMap();
+                
+        Method[] methods = bean.getClass().getMethods();
+        for( Method method : methods ) {
+            String name = method.getName();
+            if( method.getParameterTypes().length != 0 )  continue;
+            if( ! method.getReturnType().equals( String.class ) )  continue;
+            if( ! (name.startsWith("get") || name.startsWith("is")) )  continue;
+            try {
+                ret.put( StringUtils.uncapitalize( name ), (String) method.invoke(bean));
+            } catch(     IllegalAccessException | IllegalArgumentException | InvocationTargetException ex ) {
+                log.warn("Failed extracting property from " + bean.getClass().getSimpleName() + ":\n    " + ex.getMessage(), ex );
+            }
+        }
+        return ret;
+    }
 
 
 }// class
