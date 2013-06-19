3bccc0a2c45a35a07297f45d82f13670fd0f30da
OndraZizka <zizka@seznam.cz>
2013-06-19 06:33:35 +0200

    MIGR-129 Use @Property in JAXB beans and create apparatus to use that when creating ModelNode / CLI command;  Additional metadata in @Property

--- src/main/java/org/jboss/loom/spi/ann/Property.java
+++ src/main/java/org/jboss/loom/spi/ann/Property.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- *
+ *  Metadata for JAXB classes; to be used for reporting and ModelNode creation.
  *  @author Ondrej Zizka, ozizka at redhat.com
  */
 @Retention( RetentionPolicy.RUNTIME )
@@ -17,5 +17,22 @@ public @interface Property {
     public String expr() default "";
     public String label() default "";
     public String style() default "";
+
+    /**
+     *  Annotated field or method will be skipped when setting a ModelNode or creating a report.
+     */
+    @Retention( RetentionPolicy.RUNTIME )
+    @Target({ElementType.FIELD, ElementType.METHOD })
+    public static @interface Skip {}
+    
+    /**
+     *  Determines how the properties will be treated in the given class.
+     */
+    @Retention( RetentionPolicy.RUNTIME )
+    @Target({ElementType.TYPE})
+    public static @interface Access {
+        public enum Type { PUBLIC, FIELD, ANNOTATED }
+        public Type value() default Type.FIELD;
+    }
     
 }// class
