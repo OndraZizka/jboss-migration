5ba88dd8b41d27196676880f03801a41adc4f85c
OndraZizka <zizka@seznam.cz>
2013-06-20 01:34:13 +0200

    Format

--- src/main/java/org/jboss/loom/migrators/dataSources/jaxb/XaDatasourcePropertyBean.java
+++ src/main/java/org/jboss/loom/migrators/dataSources/jaxb/XaDatasourcePropertyBean.java
@@ -10,15 +10,13 @@ package org.jboss.loom.migrators.dataSources.jaxb;
 import javax.xml.bind.annotation.*;
 
 /**
- * Class for unmarshalling/marshalling and representing xa-datasource-property (AS5, AS7)
+ * JAXB bean for xa-datasource-property (AS5, AS7)
  *
  * @author Roman Jakubco
  */
-
 @XmlRootElement(name = "xa-datasource-property")
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlType(name = "xa-datasource-property")
-
 public class XaDatasourcePropertyBean {
 
     @XmlValue
@@ -27,20 +25,9 @@ public class XaDatasourcePropertyBean {
     @XmlAttribute(name = "name")
     private String xaDatasourcePropName;
 
-    public String getXaDatasourceProp() {
-        return xaDatasourceProp;
-    }
-
-    public void setXaDatasourceProp(String xaDatasourceProp) {
-        this.xaDatasourceProp = xaDatasourceProp;
-    }
-
-    public String getXaDatasourcePropName() {
-        return xaDatasourcePropName;
-    }
-
-    public void setXaDatasourcePropName(String xaDatasourcePropName) {
-        this.xaDatasourcePropName = xaDatasourcePropName;
-    }
-
-}
+    public String getXaDatasourceProp() { return xaDatasourceProp; }
+    public void setXaDatasourceProp(String xaDatasourceProp) { this.xaDatasourceProp = xaDatasourceProp; }
+    public String getXaDatasourcePropName() { return xaDatasourcePropName; }
+    public void setXaDatasourcePropName(String xaDatasourcePropName) { this.xaDatasourcePropName = xaDatasourcePropName; }
+    
+}// class
