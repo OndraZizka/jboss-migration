1232d7082cc07c79d7ac7ec20f622de7d1d61f12
OndraZizka <zizka@seznam.cz>
2013-06-24 22:10:08 +0200

    Improve MigratorSubclassMaker

--- src/main/java/org/jboss/loom/migrators/_ext/MigratorSubclassMaker.java
+++ src/main/java/org/jboss/loom/migrators/_ext/MigratorSubclassMaker.java
@@ -1,10 +1,14 @@
 package org.jboss.loom.migrators._ext;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.CtConstructor;
 import javassist.NotFoundException;
+import org.jboss.loom.conf.GlobalConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,24 +16,57 @@ import org.slf4j.LoggerFactory;
  *  Programatically creates subclasses of DefinitionBasedMigrator.
  * 
  *  @author Ondrej Zizka, ozizka at redhat.com
+ * 
+ *  TODO: Perhaps extend ClassLoader?
  */
 public class MigratorSubclassMaker {
     private static final Logger log = LoggerFactory.getLogger( MigratorSubclassMaker.class );
 
-    public static Class<? extends DefinitionBasedMigrator> createClass( String fullName )
+    
+    static Class<? extends DefinitionBasedMigrator> createClass( String fullName )
             throws NotFoundException, CannotCompileException
     {
         ClassPool pool = ClassPool.getDefault();
         
         // Create the class.
         CtClass cc = pool.makeClass( fullName );
-        cc.setSuperclass( pool.get(DefinitionBasedMigrator.class.getName()) );
+        final CtClass superClass = pool.get( DefinitionBasedMigrator.class.getName() );
+        cc.setSuperclass( superClass );
         cc.setModifiers( Modifier.PUBLIC );
+        
+        // Add a constructor which will call super( ... );
+        CtClass[] params = new CtClass[]{
+            pool.get( MigratorDefinition.class.getName() ),
+            pool.get( GlobalConfiguration.class.getName()) 
+        };
+        final CtConstructor ctor = new CtConstructor( params, cc );
+        //ctor.setBody(" super(); ");
+        ctor.setBody( superClass.getDeclaredConstructor( params ), null );
+        cc.addConstructor( ctor );
         
         // Add getter which will return the migrator id (e.g. "datasources")
         
         return cc.toClass();
     }
-    
 
+
+    /**
+     *  Instantiates given DefinitionBasedMigrator subclass and initializes it according to given migrator definition.
+     */
+    static DefinitionBasedMigrator instantiate( 
+            Class<? extends DefinitionBasedMigrator> migClass, 
+            MigratorDefinition def, 
+            GlobalConfiguration globConf 
+    )
+            throws NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException 
+    {
+        Constructor<? extends DefinitionBasedMigrator> ctor = migClass.getConstructor( MigratorDefinition.class, GlobalConfiguration.class );
+        if( ctor == null)
+            throw new IllegalArgumentException("Class doesn't have constructor ( MigratorDefinition, GlobalConfiguration ): "
+                    + migClass.getName() );
+        
+        DefinitionBasedMigrator mig = ctor.newInstance( def, globConf );
+        return mig;
+    }
+    
 }// class
