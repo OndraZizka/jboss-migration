18cbbce927a30220328e8ad05b0bbaf634e2aad8
OndraZizka <zizka@seznam.cz>
2013-06-26 04:02:08 +0200

    Improve ext mig test

--- src/test/java/org/jboss/loom/migrators/_ext/ExternalMigratorsLoaderTest.java
+++ src/test/java/org/jboss/loom/migrators/_ext/ExternalMigratorsLoaderTest.java
@@ -1,10 +1,13 @@
 package org.jboss.loom.migrators._ext;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 import org.apache.commons.io.FileUtils;
 import org.jboss.loom.conf.GlobalConfiguration;
 import org.jboss.loom.utils.Utils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -13,21 +16,27 @@ import org.junit.Test;
  */
 public class ExternalMigratorsLoaderTest {
     
-    public ExternalMigratorsLoaderTest() {
+    private File workDir;
+    
+    @BeforeClass
+    private void copyTestExtMigratorFiles() throws IOException {
+        this.workDir = new File("target/extMigrators/");
+        FileUtils.forceMkdir( workDir );
+        Utils.copyResourceToDir( ExternalMigratorsLoader.class, "TestMigrator.mig.xml", workDir );
+        Utils.copyResourceToDir( ExternalMigratorsLoader.class, "TestJaxbBean.groovy",  workDir );
     }
-
+    
+    @AfterClass
+    private void deleteTestExtMigratorFiles() throws IOException {
+        FileUtils.forceDelete( workDir );
+    }
 
     @Test
     public void testLoadMigrators() throws Exception {
         System.out.println( "loadMigrators" );
         
-        File workDir = new File("target/extMigrators/");
-        FileUtils.forceMkdir( workDir );
-        Utils.copyResourceToDir( ExternalMigratorsLoader.class, "TestMigrator.mig.xml", workDir );
-        Utils.copyResourceToDir( ExternalMigratorsLoader.class, "TestJaxbBean.groovy",  workDir );
-        
         Map<Class<? extends DefinitionBasedMigrator>, DefinitionBasedMigrator> migs
-                = new ExternalMigratorsLoader().loadMigrators( workDir, new GlobalConfiguration() );
+                = new ExternalMigratorsLoader().loadMigrators( this.workDir, new GlobalConfiguration() );
         
         for( Map.Entry<Class<? extends DefinitionBasedMigrator>, DefinitionBasedMigrator> entry : migs.entrySet() ) {
             Class<? extends DefinitionBasedMigrator> cls = entry.getKey();
@@ -35,6 +44,5 @@ public class ExternalMigratorsLoaderTest {
             System.out.println( String.format("  Loaded migrator %s: %s", cls.getName(), mig.toString() ) );
         }
     }
-
 
 }// class
