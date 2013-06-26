1ae00d4144c770662fba11f45ab50fea4fc60651
OndraZizka <zizka@seznam.cz>
2013-06-26 21:47:40 +0200

    MIGR-130 Allow using BeanValidation on JAXB classes (over HibernateValidator); add HasValidation

---
+++ src/main/java/org/jboss/loom/migrators/HasValidation.java
@@ -0,0 +1,10 @@
+package org.jboss.loom.migrators;
+
+/**
+ *  Marker interface for beans which have BeanValidation constraints (javax.validation annotations).
+ * 
+ *  @author Ondrej Zizka, ozizka at redhat.com
+ */
+public interface HasValidation {
+    
+}
