c4f05fde81bf329774564d27e2d36b9eeea129d3
OndraZizka <zizka@seznam.cz>
2013-07-31 02:24:24 +0200

    Cleanup

--- src/main/java/org/jboss/loom/conf/Configuration.java
+++ src/main/java/org/jboss/loom/conf/Configuration.java
@@ -51,22 +51,6 @@ public class Configuration {
     }// enum
 
 
-    /**
-     *  Returns all config migrator-specific values for the given migrator.
-     */
-    private MultiValueMap getForMigrator(Class<? extends IMigrator> migratorClass) {
-        //return moduleConfigs.get(migrator);
-        
-        /*
-        MultiValueMap map = new MultiValueMap();
-        IMigrator mig = ...  // or migratorClass.getMethod("", parameterTypes);
-                
-        for( ModuleSpecificProperty prop : moduleConfigs ){
-            if( 0 != mig.examineConfigProperty(prop) )
-        }*/
-        throw new UnsupportedOperationException();
-        // TODO: This method must be in a context - only the instances have the list of values they have taken.
-    }
     
     
     /**
