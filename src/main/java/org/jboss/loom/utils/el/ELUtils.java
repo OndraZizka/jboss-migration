a8d84b99722cf42977305e7aefb460860ea6c78e
OndraZizka <zizka@seznam.cz>
2013-08-03 14:24:49 +0200

    Also search for @EL in superclasses

--- src/main/java/org/jboss/loom/utils/el/ELUtils.java
+++ src/main/java/org/jboss/loom/utils/el/ELUtils.java
@@ -13,22 +13,26 @@ public class ELUtils {
     private static final Logger log = LoggerFactory.getLogger( ELUtils.class );
 
 
-    public static void evaulateObjectMembersEL( Object obj, JuelCustomResolverEvaluator eval ) {
+    public static void evaluateObjectMembersEL( Object obj, JuelCustomResolverEvaluator eval ) {
         
-        for( Field fld : obj.getClass().getDeclaredFields() ){
-            //if( ! fld.getType().equals( String.class ))
-            if( ! String.class.isAssignableFrom(  fld.getType() ) )
-                continue;
-            if( null == fld.getAnnotation( EL.class ))
-                continue;
-                
-            try {
-                String orig = (String) fld.get( obj );
-                String res = eval.evaluateEL( orig );
-                fld.set( obj, res );
-            } catch( IllegalArgumentException | IllegalAccessException ex ) {
-                throw new IllegalStateException("Failed resolving EL in " + obj + ": " + ex.getMessage(), ex);
+        Class curClass = obj.getClass();
+        while( curClass != null  &&  ! Object.class.equals( curClass ) ){
+            for( Field fld : curClass.getDeclaredFields() ){
+                //if( ! fld.getType().equals( String.class ))
+                if( ! String.class.isAssignableFrom(  fld.getType() ) )
+                    continue;
+                if( null == fld.getAnnotation( EL.class ))
+                    continue;
+
+                try {
+                    String orig = (String) fld.get( obj );
+                    String res = eval.evaluateEL( orig );
+                    fld.set( obj, res );
+                } catch( IllegalArgumentException | IllegalAccessException ex ) {
+                    throw new IllegalStateException("Failed resolving EL in " + obj + ": " + ex.getMessage(), ex);
+                }
             }
+            curClass = curClass.getSuperclass();
         }
     }
 
