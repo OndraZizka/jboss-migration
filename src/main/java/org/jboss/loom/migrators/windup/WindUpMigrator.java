05c677b7f260fd1257b4ffa441af2bb9bbbac0ed
OndraZizka <zizka@seznam.cz>
2013-09-05 06:32:48 +0200

    Imports

--- src/main/java/org/jboss/loom/migrators/windup/WindUpMigrator.java
+++ src/main/java/org/jboss/loom/migrators/windup/WindUpMigrator.java
@@ -7,7 +7,6 @@ import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import org.apache.commons.io.FileUtils;
